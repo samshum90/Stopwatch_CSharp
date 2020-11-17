41ec1c8a4387d1e183765254c25cc41ca9f4f41c
Samuel Shum <55974596+samshum90@users.noreply.github.com>
2020-11-17 17:11:54 +0000

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            while(true)
+            {
+                Console.WriteLine("Start or Stop Stopwatch?");
+                Console.WriteLine("quit to exit");
+                string input = Console.ReadLine();
+                if (input.ToLower()== "quit")
+                {
+                    Console.WriteLine("Stopwatch exited!");
+                    break;
+                }
+                if (input.ToLower() == "start")
+
+                {
+
+                    stopwatch.Start();
+
+                    continue;
+
+                }
+
+                if (input.ToLower() == "stop")
+
+                {
+
+                    stopwatch.Stop();
+
+                    Console.WriteLine("Duration - {0}", stopwatch.Duration());
+
+                    continue;
+
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _running;
+
+        public void Start()
+        {
+            if(_running)
+                throw new InvalidOperationException("Stopwatch is already running!");
+            Console.WriteLine("Start Time: {0}", _startTime = DateTime.Now);
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            if(!_running)
+                throw new InvalidOperationException("Stopwatch is already stopped!");
+            Console.WriteLine("End Time: {0}", _endTime = DateTime.Now);
+            _running = false;
+        }
+
+        public TimeSpan Duration()
+        {
+            return _endTime - _startTime;
+        }
+    }
+}
